c766898ef41590317cedac9491825aac69fa69a4
Romain Petit <romain.petit@inria.fr>
2020-11-02 01:39:18 +0100

    doc doc

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,8 @@ release = '0.1'
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.napoleon',
               'sphinx.ext.coverage',
-              'sphinx.ext.mathjax']
+              'sphinx.ext.mathjax',
+              'sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
